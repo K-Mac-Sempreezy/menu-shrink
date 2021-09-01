3518aa28c07a374429cc7d0984bfe5470c2bd4a9
Kristian MacDonald <kristian.macsempreezy@gmail.com>
2021-09-01 13:23:57 -0700

    attempt to fix output bundle syntax

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,6 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
-  output: {
-    filename: './bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
-  },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
@@ -34,5 +28,11 @@ module.exports = {
         type: 'asset/resource',
       }
     ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
   },
 };
